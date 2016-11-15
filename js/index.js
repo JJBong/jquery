d189db77942ab2229948380b5f9c87d70cc788ee
KimJubong <jubong1992@gmail.com>
2016-11-15 09:57:59 +0900

    second commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by kim on 2016-11-15.
+ */
+
+var test1 = 100;
+var test2 = true;
+var test3 = 'h';
+var test4 = null;
+var test5;
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
